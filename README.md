463ecca44ae19278eebf4ef32d3aa80f5fa4f801
Joe Mahoney <jmahoney@iterative.co.nz>
2014-07-27 21:20:01 +1200

    source file and script run by cron

---
+++ README.md
@@ -0,0 +1,3 @@
+#### every-eggs-and-ham
+
+a twitter bot celebrating @everyword's "task complete".
---
+++ toot.sh
@@ -0,0 +1,4 @@
+!#/bin/sh
+python everywordbot.py --consumer_key=Tp8pQaaI4NkTMUJV6F1YcHYPk --consumer_secret=ZYyWBcincQ73sAmkEpwX7Ucv5ME6PaxaZxL7R6wzgDycB6rJU8 \
+      --access_token=2554389931-cEViK2Rgq9N26Z4Y0rjdhjgwWk3g8s9LVKgG2By --token_secret=2x36dws9SRlA1BYNfVjXPp9w4MaLR1SwI7SLhaa130fHy \
+      --source_file=source.txt --index_file=index.txt
